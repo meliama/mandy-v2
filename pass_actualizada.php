5a1cc89aea5bf0b5b80608324628d6246d1cf859
meliama <meli.amaike@gmail.com>
2017-10-22 13:41:35 -0300

    add pass_actualizada.php

---
+++ pass_actualizada.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/normalize.css" rel="stylesheet">
+    <link href="css/ionicons-2.0.1/css/ionicons.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>Mandy.com - Compra arte independiente local</title>
+  </head>
+  <body>
+
+    <!-- HEADER -->
+
+   <header class="main-header">
+
+     <input type="checkbox" id="open-nav">
+
+     <label for="open-nav" class="toggle-nav">
+       <span class="ion-navicon-round"></span>
+     </label>
+
+     <a href="index.html" class="logo-title">
+       <h1>Mandy</h1>
+     </a>
+
+     <!-- NAV -->
+
+     <nav class="main-nav">
+
+       <label class="close-nav" for="open-nav">
+         <span class="ion-chevron-left"></span>
+       </label>
+
+       <a href="#" class="shopping-bag sb-mobile">
+         <span class="ion-bag"></span>
+       </a>
+
+       <ul>
+         <li><a href="index.html">Inicio</a></li>
+         <li><a href="#">Cómo funciona</a></li>
+         <li><a href="#">Categorías</a></li>
+         <li><a href="#">Servicios</a></li>
+         <li><a href="faqs.html">FAQs</a></li>
+       </ul>
+
+       <span class="registro-mobile">
+         <a href="login.php" class="log-in-mobile">Login</a>
+         <a href="registro.php">
+           ¿Aún no tienes cuenta?<br><u>Regístrate.</u>
+         </a>
+       </span>
+
+     </nav>
+
+     <div class="icon-nav">
+
+       <input type="checkbox" id="open-search">
+
+       <div class="links-desktop">
+         <a href="registro.php">Regístrate</a>
+         <a href="login.php" class="log-in-desktop">Login</a>
+       </div>
+
+       <form action="" method="get" class="top-searchbar">
+         <input type="text" name="top-searchbar" placeholder="¿Qué estás buscando?">
+         <!-- <input type="submit" name="search" value="Ir"> -->
+       </form>
+
+       <label for="open-search" class="top-search">
+           <span class="ion-search"></span>
+       </label>
+
+       <a href="#" class="shopping-bag sb-desktop">
+         <span class="ion-bag"></span>
+       </a>
+
+     </div>
+
+   </header>
+
+
+    <div class="page-container login-registro-content">
+
+      <div class="titulo-login">
+          <h3>¿Olvidaste tu contraseña?</h3>
+      </div>
+   <span >
+     <h3 class="">Tu password ha sido actualizada.</h3>
+   <a href="index.html" class="boton-volver">Volver</a>
+ </span>
+
+
+
+
+
+
+
+    </div>
+
+  </body>
+</html>
